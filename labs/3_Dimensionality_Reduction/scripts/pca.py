5ff7bb1f181a194e05ee5374b609de1220a1d615
Giuseppe Ruggeri <ruggerigiuseppe_work@hotmail.com>
2021-03-23 17:09:35 +0100

    lab3 - pca completed and working

--- labs/3_Dimensionality_Reduction/scripts/pca.py
+++ labs/3_Dimensionality_Reduction/scripts/pca.py
@@ -5,15 +5,35 @@ sys.path.append('../../2_Iris/scripts/')
 
 import iris_load_visualize as ilv
 
-attributes, labels = ilv.loadDataset(ilv.dsPath)
-print(attributes.shape)
-mu = attributes.mean(1)
-mu = mu.reshape(mu.size, 1)
-# Centering dataset
-DC = attributes - mu
+def pca(D, m):
+    mu = D.mean(1)
+    mu = mu.reshape(mu.size, 1)
+    # Centering dataset using broadcasting
+    DC = D - mu
 
-# Covariance matrix
-C = (np.dot(DC, DC.T))/DC.shape[1]
+    # Covariance matrix
+    C = (np.dot(DC, DC.T))/DC.shape[1]
+    # Extracting eigenvectors and eigenvalues from a symmetric matrix
+    s, U = np.linalg.eigh(C)
 
-print(mu)
-print(C)
+    # Extracting the m best eigenvectors (corresponding to the m greatest eigenvalues)
+    P = U[:, ::-1][:, 0:m]
+
+    # Projection of dataset to new directions
+    DP = np.dot(P.T, DC)
+
+    #print("Eigenvectors: ")
+    #print(P)
+
+    return P, DP
+
+if (__name__ == "__main__"):
+    attributes, labels = ilv.loadDataset(ilv.dsPath)
+    P, DP = pca(attributes, 4)
+
+    print("Eigenvectors calculated:")
+    print(P)
+
+    Pld = np.load("../Solution/IRIS_PCA_matrix_m4.npy")
+    print("\nEigenvectors from the solution:")
+    print(Pld)
