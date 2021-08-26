cc787010cc7850219fac79a97f9deb566063f6fb
Giuseppe Ruggeri <ruggerigiuseppe_work@hotmail.com>
2021-08-26 19:34:27 +0200

    lab 8 finished up and working in every part; implemented multiclass logistic regression

--- classifiers/logistic_regression.py
+++ classifiers/logistic_regression.py
@@ -1,5 +1,25 @@
 import numpy as np
 import scipy.optimize
+import scipy.special
+
+class LogisticRegressionClassifier:
+    def __init__(self):
+        self.DTR = None
+        self.LTR = None
+        self.l = 0
+        self.w = None
+        self.b = None
+
+    def train(self, DTR, LTR, l, maxfun=15000, maxiter=15000, verbose=0):
+        self.DTR = DTR
+        self.LTR = LTR
+        self.l = l
+
+        self.w, self.b = LR_Classifier_train(DTR, LTR, l, maxfun, maxiter, verbose)
+
+    def inference(self, D):
+        pred_labels = LR_Classifier_inference(D, self.w, self.b)
+        return pred_labels
 
 def logreg_obj_wrapper(DTR, LTR, l):
     def logreg_obj(v):
@@ -23,44 +43,65 @@ def LR_Classifier_train(DTR, LTR, l, maxfun=15000, maxiter=15000, verbose=0):
     if verbose:
         print("Training Logistic Regression classifier using Average Risk Minimizer method..")
         print("Lambda regularizer: ", l)
-        print("Max function calls: ", maxfun)
-        print("Max iterations: ", maxiter)
+        #print("Max function calls: ", maxfun)
+        #print("Max iterations: ", maxiter)
 
-    logreg_obj = logreg_obj_wrapper(DTR, LTR, l)
-    x0 = np.zeros(DTR.shape[0] + 1)
+    D = DTR.shape[0]
+    K = len(set(LTR))
+    # Multiclass or binary LR
+    if K > 2:
+        logreg_obj = logreg_multiclass_obj_wrapper(DTR, LTR, l)
+        x0 = np.zeros(D*K + K)
+    else:
+        logreg_obj = logreg_obj_wrapper(DTR, LTR, l)
+        x0 = np.zeros(D + 1)
+
     xMin, fMin, d = scipy.optimize.fmin_l_bfgs_b(logreg_obj, x0, maxfun=maxfun, maxiter=maxiter, approx_grad=True)
 
-    w, b = xMin[0:-1], xMin[-1]
+    if K > 2:
+        w, b = xMin[0:D*K].reshape((D, K)), xMin[D*K:].reshape((K, 1))
+    else:
+        w, b = xMin[0:-1].T, xMin[-1]
     if verbose:
-        print("Logistic Regression W direction and bias b:")
-        print("w*: ", w)
-        print("b*: ", b)
+        #print("Logistic Regression W direction and bias b:")
+        #print("w*: ", w)
+        #print("b*: ", b)
         print("J(w*, b*): ", fMin)
 
-    return w.T, b
+    return w, b
 
 def LR_Classifier_inference(D, w, b):
+    K = w.shape[1]
     S = w.T @ D + b
 
-    pred_labels = np.array([1 if score > 0 else 0 for score in S])
+    if K > 2:
+        pred_labels = np.argmax(S, 0)
+    else:
+        pred_labels = np.array([1 if score > 0 else 0 for score in S])
     return pred_labels
 
-class LogisticRegressionClassifier:
-    def __init__(self):
-        self.DTR = None
-        self.LTR = None
-        self.l = 0
-        self.w = None
-        self.b = None
+# MULTICLASS LR
+def logreg_multiclass_obj_wrapper(DTR, LTR, l):
+    def logreg_multiclass_obj(v):
+        D = DTR.shape[0]    # dimensionality of feature
+        K = len(set(LTR))   # number of classes
 
-    def train(self, DTR, LTR, l, maxfun=15000, maxiter=15000, verbose=0):
-        self.DTR = DTR
-        self.LTR = LTR
-        self.l = l
+        W = v[0:D*K].reshape((D, K))
+        b = v[D*K:].reshape((K, 1))
+        regularization_term = 0.5 * l * (W*W).sum()
 
-        self.w, self.b = LR_Classifier_train(DTR, LTR, l, maxfun, maxiter, verbose)
+        # Score matrix for all samples
+        S = W.T @ DTR + b
+        Ylog = S - scipy.special.logsumexp(S, axis=0)
 
-    def inference(self, D):
-        pred_labels = LR_Classifier_inference(D, self.w, self.b)
-        return pred_labels
+        # 1-of-K encoding for labels
+        T = np.zeros((K, LTR.shape[0]))
+        for index, label in enumerate(LTR):
+            T[label, index] = 1
 
+        J = (T * Ylog).sum()
+        J = -J / DTR.shape[1] + regularization_term
+
+        return J
+
+    return logreg_multiclass_obj
---
+++ labs/8_LogisticRegression/scripts/multiclass_logistic_regression.py
@@ -0,0 +1,27 @@
+import numpy as np
+import sklearn.datasets
+
+import preproc.dstools as dst
+from classifiers.logistic_regression import LogisticRegressionClassifier
+
+def load_iris():
+    D, L = sklearn.datasets.load_iris()['data'].T, sklearn.datasets.load_iris()['target']
+    return D, L
+
+
+if __name__ == "__main__":
+    D, L = load_iris()
+    (DTR, LTR), (DTE, LTE) = dst.split_db_2to1(D, L, 2.0/3.0)
+
+    lambdas = [0, 1e-6, 1e-3, 1.0]
+    for l in lambdas:
+        print("Lambda: ", l)
+        lr_model = LogisticRegressionClassifier()
+        lr_model.train(DTR, LTR, l, verbose=1)
+        pred_labels = lr_model.inference(DTE)
+
+        correct_predictions = (pred_labels == LTE).sum()
+        accuracy = correct_predictions / pred_labels.shape[0]
+        print("Accuracy: ", accuracy)
+        print("Error rate: ", 1 - accuracy)
+        print("\n")
