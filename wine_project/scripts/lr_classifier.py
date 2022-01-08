aad1b65567e0c9cc31a3a6f262c5e67ac7e169ad
Giuseppe Ruggeri <ruggerigiuseppe_work@hotmail.com>
2022-01-09 00:44:50 +0100

    logistic regression classifier parametrized and new train logs-graphs generated

--- wine_project/scripts/lr_classifier.py
+++ wine_project/scripts/lr_classifier.py
@@ -6,6 +6,8 @@ import evaluation.common as eval
 
 from classifiers.logistic_regression import cross_validate_lr
 
+import argparse
+
 SCRIPT_PATH = os.path.dirname(__file__)
 TRAINLOGS_BASEPATH = os.path.join(SCRIPT_PATH, "..", "train_logs", "lr")
 LINEAR_LR_TRAINLOG_FNAME = "linear_lr_trainlog_1.txt"
@@ -15,14 +17,28 @@ QUADRATIC_LR_BEST_TRAINLOG_FNAME = "quadratic_lr_best_trainlog_1.txt"
 LINEAR_LR_GRAPH_PATH = os.path.join(SCRIPT_PATH, "..", "graphs", "lr", "linear_lr_graph_")
 QUADRATIC_LR_GRAPH_PATH = os.path.join(SCRIPT_PATH, "..", "graphs", "lr", "quadratic_lr_graph_")
 
-# This script has to be called with one (or more) command-line parameters among
-# {linear, quadratic, linear_best, quadratic_best}
-# in order to start the requested analysis.
-# It produces as output some plot graphs (one for each tried configuration)
-# under the folder /graphs comparing minDCFs for different lambda regularizers
+def get_args():
+    parser = argparse.ArgumentParser(description="Script to launch Logistic Regression classificator building",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("--linear", type=bool, default=False,
+                        help="Start cross-validation for the linear regularized logistic regression model")
+    parser.add_argument("--quadratic", type=bool, default=False,
+                        help="Start cross-validation for the quadratic regularized logistic regression model")
+    parser.add_argument("--linear_app_specific", type=bool, default=False,
+                        help="Start cross-validation for the linear regularized logistic regression model trained for the specific target applications")
+    parser.add_argument("--quadratic_app_specific", type=bool, default=False,
+                        help="Start cross-validation for the quadratic regularized logistic regression model trained for the specific target applications")
+
+    return parser.parse_args()
+
+# It produces as output some plot graphs (one for each tried configuration) against lambda
+# under the folder /graphs/lr comparing minDCFs for different lambda regularizers
 # for the different target applications. It also produces a training log
-# for each analysis under the folder /train_logs
+# for each analysis under the folder /train_logs/lr
 if __name__ == "__main__":
+    args = get_args()
+
     # Load 5-Folds already splitted dataset
     folds_data, folds_labels = load_train_dataset_5_folds()
 
@@ -91,6 +107,12 @@ if __name__ == "__main__":
         return minDCFs
 
     def lr_gridsearch(specific_pi1=None, quadratic=False):
+        """
+        It produces as output some plot graphs (one for each tried configuration) against lambda
+        under the folder /graphs/lr comparing minDCFs for different lambda regularizers
+        for the different target applications. It also produces a training log
+        for each analysis under the folder /train_logs/lr
+        """
         pi1_str = "with prior weight specific training (π=%.1f)" % (specific_pi1) if specific_pi1 is not None else ""
         print("%s Logistic Regression %s analysis started" % ("Quadratic" if quadratic else "Linear", pi1_str))
         total_time_start = time.perf_counter()
@@ -109,35 +131,69 @@ if __name__ == "__main__":
         print("LR analysis finished. Plots saved in %s. Total time: %d seconds.\n\n" %
               (QUADRATIC_LR_GRAPH_PATH if quadratic else LINEAR_LR_GRAPH_PATH, total_time_end - total_time_start))
 
-    args = sys.argv[1:]
-
-    if "linear" in args:
+    if args.linear:
         with LoggingPrinter(incremental_path(TRAINLOGS_BASEPATH, LINEAR_LR_TRAINLOG_FNAME)):
-            lr_gridsearch(quadratic=False)
-    if "quadratic" in args:
+            lr_gridsearch(specific_pi1=applications[0][0], quadratic=False)
+    if args.quadratic:
         with LoggingPrinter(incremental_path(TRAINLOGS_BASEPATH, QUADRATIC_LR_TRAINLOG_FNAME)):
-            lr_gridsearch(quadratic=True)
+            lr_gridsearch(specific_pi1=applications[0][0], quadratic=True)
 
     # Best hyperparameters
-    linear_preproc_conf = preproc_configurations[0] # TODO
-    linear_lambda = 0.01 # TODO
+    # Plug manually the best preprocess configuration and the best lambda to try training for application specific scenarios
+    # Linear
+    # Pi = 0.1
+    linear_preproc_conf_pi01 = preproc_configurations[2]
+    linear_lambda_pi01 = 0.01
+    # Pi = 0.9
+    linear_preproc_conf_pi09 = preproc_configurations[5]
+    linear_lambda_pi09 = 0.00001
 
-    quadratic_preproc_conf = preproc_configurations[0]  # TODO
-    quadratic_lambda = 0.01  # TODO
+    # Quadratic
+    quadratic_preproc_conf_pi01 = preproc_configurations[2]
+    quadratic_lambda_pi01 = 0.001
+    # Pi = 0.9
+    quadratic_preproc_conf_pi09 = preproc_configurations[6]
+    quadratic_lambda_pi09 = 0.01
 
     # Train new models with the best hyper parameters but embedding a
     # specific prior = application prior in the training process
-    lambdas = np.logspace(-5, 3, 9)
+    def _cross_validate_lr(conf, l, pi1=None, quadratic=False):
+        time_start = time.perf_counter()
+        scores, labels = cross_validate_lr(folds_data, folds_labels, conf, l, pi1=pi1, quadratic=quadratic)
+        minDCFs = []
+        for app_i, (pi1, Cfn, Cfp) in enumerate(applications):
+            minDCF, _ = eval.bayes_min_dcf(scores, labels, pi1, Cfn, Cfp)
+            print("\t\tmin DCF (π=%.1f) : %.3f" % (pi1, minDCF))
+            minDCFs.append(minDCF)
+        time_end = time.perf_counter()
+        print("\t\ttime passed: %d seconds" % (time_end - time_start))
+
+        return minDCFs
+
     # LINEAR LR
-    if "linear_best" in args:
+    if args.linear_app_specific:
         with LoggingPrinter(incremental_path(TRAINLOGS_BASEPATH, LINEAR_LR_BEST_TRAINLOG_FNAME)):
-            for (pi1, Cfn, Cfp) in applications:
-                lr_plot_against_lambda(linear_preproc_conf, lambdas, pi1, False)
+            print(f"Cross-validating Linear Logistic Regression with the best preproc configuration and the best lambda embedding target application prior in the training process..")
+            pi01 = applications[1][0]
+            print(f"\tLinear Logistic Regression training for target application specific π={pi01:.1f}..")
+            _cross_validate_lr(linear_preproc_conf_pi01, linear_lambda_pi01, pi1=pi01, quadratic=False)
+
+            pi09 = applications[2][0]
+            print(f"\tLinear Logistic Regression training for target application specific π={pi09:.1f}..")
+            _cross_validate_lr(linear_preproc_conf_pi09, linear_lambda_pi09, pi1=pi09, quadratic=False)
+
+    print("\n")
 
     # QUADRATIC LR
-    if "quadratic_best" in args:
+    if args.quadratic_app_specific:
         with LoggingPrinter(incremental_path(TRAINLOGS_BASEPATH, QUADRATIC_LR_BEST_TRAINLOG_FNAME)):
-            for (pi1, Cfn, Cfp) in applications:
-                lr_plot_against_lambda(linear_preproc_conf, lambdas, pi1, True)
+            print(f"Cross-validating Quadratic Logistic Regression with the best preproc configuration and the best lambda embedding target application prior in the training process..")
+            pi01 = applications[1][0]
+            print(f"\tQuadratic Logistic Regression training for target application specific π={pi01:.1f}..")
+            _cross_validate_lr(quadratic_preproc_conf_pi01, quadratic_lambda_pi01, pi1=pi01, quadratic=True)
+
+            pi09 = applications[2][0]
+            print(f"\tQuadratic Logistic Regression training for target application specific π={pi09:.1f}..")
+            _cross_validate_lr(quadratic_preproc_conf_pi09, quadratic_lambda_pi09, pi1=pi09, quadratic=True)
 
     plt.show()
