3f9f94524ec7d7a98f3815030902f15b1589ae88
Giuseppe Ruggeri <ruggerigiuseppe_work@hotmail.com>
2022-01-10 19:28:51 +0100

    refactored gmm classifier and added rbf_svm actual dcf trainlog

--- classifiers/gmm_classifier.py
+++ classifiers/gmm_classifier.py
@@ -2,6 +2,8 @@ import numpy as np
 import scipy.special
 
 import classifiers.distributions.gaussian as gau
+import preproc.dstools as dst
+from density_estimation.gaussian_mixture_model import LBG_estimate
 
 class GMM_Classifier:
 
@@ -24,3 +26,51 @@ class GMM_Classifier:
 
     def compute_binary_llr(self, D):
         return (gau.logpdf_GMM(D, self.gmms[1]) - gau.logpdf_GMM(D, self.gmms[0])).flatten()
+
+def cross_validate_gmm(folds_data, folds_labels, preproc_conf, alpha, psi, diag_cov, tied_cov, max_components, verbose=False):
+    """
+    Cross-validate log2(max_components) different GMM classifiers (1 comp, 2 comps, 4 comps, 8 comps, etc..) using
+    LBG estimation and a K-fold cross-validation for a given preprocess configuration
+    and a given model configuration (tied, diag, etc.).
+    :return a tuple (scores, labels) where scores and labels are two nparray of shape (#classifiers, #validation_samples)
+            where #classifiers = log2(max_components) + 1 and #validation_samples = #folds * fold_size
+    """
+    if verbose:
+        diag_cov_str = "Diagonal" if diag_cov else ""
+        tied_cov_str = "Tied" if tied_cov else ""
+        print("\t5-Fold Cross-Validation {} {} GMM (components from 1 to {}) - Preprocessing: {}".format(
+            diag_cov_str, tied_cov_str, max_components, preproc_conf))
+    iterations = 1
+    K = folds_data.shape[0]
+    nk = folds_labels.shape[1]
+    scores = np.zeros((max_components, K*nk))
+    labels = np.zeros((max_components, K*nk))
+    k = 0
+    for DTR, LTR, DTE, LTE in dst.kfold_generate(folds_data, folds_labels):
+        # Preprocess data
+        DTR, DTE = preproc_conf.apply_preproc_pipeline(DTR, LTR, DTE)
+
+        # Train all the gmms from 1 component to max_components components for class 0 and 1
+        DTR0 = DTR[:, (LTR == 0)]
+        DTR1 = DTR[:, (LTR == 1)]
+        gmms_h0 = LBG_estimate(DTR0, alpha, psi=psi, diag_cov=diag_cov, tied_cov=tied_cov,
+                                   stop_condition_fun=(lambda curr_gmm: len(curr_gmm) == max_components), verbose=0)
+        gmms_h1 = LBG_estimate(DTR1, alpha, psi=psi, diag_cov=diag_cov, tied_cov=tied_cov,
+                                   stop_condition_fun=(lambda curr_gmm: len(curr_gmm) == max_components), verbose=0)
+        gmm_classifiers = []
+        for g0, g1 in zip(gmms_h0, gmms_h1):
+            gmms = [g0, g1]
+            gmm_classifiers.append(GMM_Classifier(gmms))
+
+        # Now gmm_classifiers contains log2(max_components) + 1 gmm classifiers
+
+        # Validate
+        for i, gmm_classifier in enumerate(gmm_classifiers):
+            s = gmm_classifier.compute_binary_llr(DTE)
+            scores[i, k*nk:(k+1)*nk] = s
+            labels[i, k*nk:(k+1)*nk] = LTE
+
+        k += 1
+        iterations += 1
+
+    return scores, labels
--- wine_project/scripts/gmm_classifier.py
+++ wine_project/scripts/gmm_classifier.py
@@ -7,7 +7,7 @@ from wine_project.utility.ds_common import *
 import evaluation.common as eval
 
 from density_estimation.gaussian_mixture_model import LBG_estimate
-from classifiers.gmm_classifier import GMM_Classifier
+from classifiers.gmm_classifier import cross_validate_gmm
 
 import argparse
 
@@ -22,6 +22,8 @@ def get_args():
 
     parser.add_argument("--gridsearch", type=bool, default=False,
                         help="Start a gridsearch cross-validation to optimize with respect to the number of components, the preprocess configuration and the model type")
+    parser.add_argument("--actual_dcf", type=bool, default=False,
+                        help="Calculate actual DCF for the different target application using the best model")
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -30,45 +32,9 @@ if __name__ == "__main__":
     # Load 5-Folds already splitted dataset
     folds_data, folds_labels = load_train_dataset_5_folds()
 
-    def cross_validate_gmm(preproc_conf, alpha, psi, diag_cov, tied_cov, max_components):
-        diag_cov_str = "Diagonal" if diag_cov else ""
-        tied_cov_str = "Tied" if tied_cov else ""
-        print("\t5-Fold Cross-Validation {} {} GMM (components from 1 to {}) - Preprocessing: {}".format(
-            diag_cov_str, tied_cov_str, max_components, preproc_conf))
-        iterations = 1
-        K = folds_data.shape[0]
-        nk = folds_labels.shape[1]
-        scores = np.zeros((max_components, K*nk))
-        labels = np.zeros((max_components, K*nk))
-        k = 0
-        for DTR, LTR, DTE, LTE in dst.kfold_generate(folds_data, folds_labels):
-            # Preprocess data
-            DTR, DTE = preproc_conf.apply_preproc_pipeline(DTR, LTR, DTE)
-
-            # Train all the gmms from 1 component to max_components components for class 0 and 1
-            DTR0 = DTR[:, (LTR == 0)]
-            DTR1 = DTR[:, (LTR == 1)]
-            gmms_h0 = LBG_estimate(DTR0, alpha, psi=psi, diag_cov=diag_cov, tied_cov=tied_cov,
-                                       stop_condition_fun=(lambda curr_gmm: len(curr_gmm) == max_components), verbose=0)
-            gmms_h1 = LBG_estimate(DTR1, alpha, psi=psi, diag_cov=diag_cov, tied_cov=tied_cov,
-                                       stop_condition_fun=(lambda curr_gmm: len(curr_gmm) == max_components), verbose=0)
-            gmm_classifiers = []
-            for g0, g1 in zip(gmms_h0, gmms_h1):
-                gmms = [g0, g1]
-                gmm_classifiers.append(GMM_Classifier(gmms))
-
-            # Now gmm_classifiers contains log2(max_components) gmm classifiers
-
-            # Validate
-            for i, gmm_classifier in enumerate(gmm_classifiers):
-                s = gmm_classifier.compute_binary_llr(DTE)
-                scores[i, k*nk:(k+1)*nk] = s
-                labels[i, k*nk:(k+1)*nk] = LTE
-
-            k += 1
-            iterations += 1
-
-        return scores, labels
+    # constants
+    ALPHA = 0.1
+    PSI = 0.01
 
     def gmm_gridsearch():
         # Preprocessing configurations to try
@@ -112,7 +78,7 @@ if __name__ == "__main__":
                 for t_i, tied in enumerate(tieds):
                     print("Grid search iteration %d / %d" % (grid_search_iterations, tot_gs_iterations_required))
                     time_start = time.perf_counter()
-                    scores, labels = cross_validate_gmm(conf, 0.1, 0.01, diag, tied, max_comps)
+                    scores, labels = cross_validate_gmm(folds_data, folds_labels, conf, ALPHA, PSI, diag, tied, max_comps, verbose=True)
                     for ci, c in enumerate(comps):
                         for app_i, (pi1, Cfn, Cfp) in enumerate(applications):
                             minDCF, _ = eval.bayes_min_dcf(scores[ci], labels[ci], pi1, Cfn, Cfp)
